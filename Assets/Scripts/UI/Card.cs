58c338bda9e55cf3665b9baf55d6c4a0da828830
RealFighter64 <realfighter64@gmail.com>
2017-03-18 18:05:39 +0000

    Import assets, beginning of game

---
+++ Assets/Scripts/UI/Card.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+	public float scaleFactor;
+	public float speedOfGrowth;
+
+	private string _name;
+	private string _description;
+	private int _manaCost;
+
+	private Text nameText;
+	private Text descriptionText;
+	private Text manaCostText;
+
+	public string Name {
+		get {
+			return this._name;
+		}
+		set {
+			_name = value;
+			UpdateCardInformation ();
+		}
+	}
+
+	public string Description {
+		get {
+			return this._description;
+		}
+		set {
+			_description = value;
+			UpdateCardInformation ();
+		}
+	}
+
+	public int ManaCost {
+		get {
+			return this._manaCost;
+		}
+		set {
+			_manaCost = value;
+			UpdateCardInformation ();
+		}
+	}
+
+	private float currentScaleFactor = 1;
+	private bool currentlyMouseOver = false;
+	public bool ready = false;
+
+	private RectTransform rectTransform;
+
+	// Use this for initialization
+	void Start () {
+		rectTransform = GetComponent<RectTransform> ();
+		nameText = transform.FindChild ("Name").GetComponent<Text> ();
+		descriptionText = transform.FindChild ("Description").GetComponent<Text> ();
+		manaCostText = transform.FindChild ("Mana Cost").GetComponent<Text> ();
+	}
+
+	public void Init (string name, string description, int manaCost) {
+		if (ready) {
+			_name = name;
+			_description = description;
+			_manaCost = manaCost;
+			UpdateCardInformation ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rectTransform.localScale = new Vector3 (currentScaleFactor, currentScaleFactor, currentScaleFactor);
+		if (currentlyMouseOver) {
+			if (currentScaleFactor < scaleFactor) {
+				currentScaleFactor += speedOfGrowth;
+			}
+		} else {
+			if (currentScaleFactor > 1) {
+				currentScaleFactor -= speedOfGrowth;
+			}
+		}
+	}
+
+	void UpdateCardInformation() {
+		nameText.text = _name;
+		descriptionText.text = _description;
+		manaCostText.text = _manaCost.ToString();
+		Debug.Log (nameText.text);
+	}
+
+	public void OnPointerEnter(PointerEventData eventData) {
+		currentlyMouseOver = true;
+	}
+
+	public void OnPointerExit(PointerEventData eventData) {
+		currentlyMouseOver = false;
+	}
+}
---
+++ Assets/Scripts/UI/Deck.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour {
+
+	public Card[] cardArray;
+
+	public Transform cardPrefab;
+	public int initialCardAmount;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < initialCardAmount; i++) {
+			cardArray = new Card[10];
+			Transform tempCard = Instantiate (cardPrefab) as Transform;
+			tempCard.SetParent (this.transform);
+			tempCard.GetComponent<RectTransform> ().localPosition = new Vector3 (-10 + -138*i, 10);
+			Card tempCardData = tempCard.GetComponent<Card> ();
+			tempCardData.Init ("aisdogdfhg", "soipdfuguusoudfygusdfygoiudfgusodifg", i);
+			cardArray [i] = tempCardData;
+		}
+
+	}
+
+	void Awake() {
+		
+	}
+}
